da2cd9b6faf9319c293e67c3279e2ddfba349243
SivaPrasadreddyevuri <evurisivaprasadreddy3@gmail.com>
2024-12-19 17:06:44 +0530

    task

--- index.js
+++ index.js
@@ -22,3 +22,16 @@ let obj={
 alert(`${obj.name} has purchased ${obj.product} which costs ${obj.price} on ${obj.day} on ${obj.occassion} in ${obj.platform}`)
 //  let arr=[1,2,3,4,5,6,7,8]
 // console.log(`smallest number in arr is ${arr[0]}and largest number in arr is ${arr[arr.length-1]}`)
+let student=prompt("Enter Your Name");
+let company=prompt("Company");
+let domain=prompt("domain");
+let package=prompt("package");
+let joindate=prompt("joindate");
+let obj1={
+    sn:student,
+    cn:company,
+    domain:domain,
+    package:package,
+    joindate:joindate
+}
+alert(`${obj1.sn}got placed in ${obj1.cn} as a ${obj1.domain}with package of ${obj1.package} and the joining date is on coming ${obj1.joindate}`)
