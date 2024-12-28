39f1e81cf001722cdd536f93187a86086e18b66b
SivaPrasadreddyevuri <evurisivaprasadreddy3@gmail.com>
2024-12-28 12:26:39 +0530

    operators

---
+++ index.js
@@ -0,0 +1,17 @@
+// let full=["html","css","bootstrap","js","node","react"];
+// let d=["html","css","bootstrap"]
+// let f=["html","css","bootstrap","js"]
+// let b=["html","css","bootstrap","js","node"]
+// let student;
+// student=console.log(d.length)>console.log(full.length)?
+// let user = prompt("Veg/ Non-veg");
+// let item = prompt("Enter your  dish (Gobi/Paneer/Chicken/Mutton)");
+// user == "veg"
+//   ? item == "Gobi"
+//     ? console.log("Gobi")
+//     : console.log("panner")
+//     : item=="Chicken"
+//   ? console.log("chicken")
+//   : console.log("mutton");
+// let a=10
+// console.log(a);
---
+++ task.js
@@ -0,0 +1,15 @@
+let a=10
+
+// let b=5
+// let result=!true;
+// console.log(a++)
+// console.log()
+// console.log(b)
+// let b=a++;
+// console.log(a)
+// console.log(b)
+// a=10;b=a<5?console.log("yes"):console.log(b)
+let obj={
+    a:{b:'c'}
+}
+console.log(obj.b.c)
