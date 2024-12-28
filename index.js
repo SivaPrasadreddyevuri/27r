ad326e9d001b370d270e96c56a7caa9154f4074c
SivaPrasadreddyevuri <evurisivaprasadreddy3@gmail.com>
2024-12-28 15:59:42 +0530

    op

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// let full=["html","css","bootstrap","js","node","react"];
-// let d=["html","css","bootstrap"]
-// let f=["html","css","bootstrap","js"]
-// let b=["html","css","bootstrap","js","node"]
-// let student;
-// student=console.log(d.length)>console.log(full.length)?
+
 // let user = prompt("Veg/ Non-veg");
 // let item = prompt("Enter your  dish (Gobi/Paneer/Chicken/Mutton)");
 // user == "veg"
--- task.js
+++ task.js
@@ -1,15 +1,17 @@
-let a=10
+let student=prompt("Check Your Eligibility for S/W Developer html,css,bootstrap,js,node,react");
+student==="html,css,bootstrap"? alert("UI/UX Designer")
+:student==="html,css,bootstrap,js"? alert("Front End Developer")
+:student==="html,css,bootstrap,js,node"? alert("Back End Developer")
+:student==="html,css,bootstrap,js,node,react"? alert("Full Stack Developr"):
+alert("let's J0in 10000k Coders to became Full-Stack Developer");
 
-// let b=5
-// let result=!true;
-// console.log(a++)
-// console.log()
-// console.log(b)
-// let b=a++;
-// console.log(a)
-// console.log(b)
-// a=10;b=a<5?console.log("yes"):console.log(b)
-let obj={
-    a:{b:'c'}
-}
-console.log(obj.b.c)
+// let student=["html","css","bootstrap","js","node","react"]
+// let d=["html","css","bootstrap",]
+// let f=["html","css","bootstrap","js"]
+// let b=["html","css","bootstrap","js","node"]
+// let full=["html","css","bootstrap","js","node","react"]
+
+//  student.length-3==d.length?console.log("UI/UX Designer")
+// :student.length-2==f.length?console.log("Front-end Developer")
+// :student.length-1==b.length?console.log("Back-end Developer")
+// :student===full?console.log("Full Stack Developer"):console.log("join 10k Coders to Became Full Stack Developer");
