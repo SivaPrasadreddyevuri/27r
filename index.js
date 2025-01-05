6f3350a6beb65ef474fd6a8f912c926f0c6c9b36
SivaPrasadreddyevuri <evurisivaprasadreddy3@gmail.com>
2025-01-05 15:31:52 +0530

    new

--- index.js
+++ index.js
@@ -5,34 +5,18 @@ let students=[
     {id:4,s_name:"rahul",city:"bengalore"},
     {id:5,s_name:"sita",city:"chennai"},
     {id:6,s_name:"vijay",city:"mumbai"}
-];
+]
+//by using for in and for of loops
 
-for(let i in students){
-    console.log(students[i].s_name,"from",students[i].city)
-}
-for(let i of students){
+
+//for-in
+// for(let i in students)
+// {
+//     // console.log(students[i].s_name,"from",students[i].city);
+//     // console.log(i);
+// }
+
+//for-of
+ for(let i of students){
     console.log(i.s_name,"from",i.city)
-}
-
-// for(i=1;i<=10;i++){
-//     console.log(`5x${i}=${i*5}`)
-// }
-// for(i=20;i>=1;i--){
-//     console.log(`15x${i}=${15*i}`)
-// }
-// let str="welcome"
-// // using for loop print each character line by line.
-// // for(i=0; i<str.length;i++){
-// //     console.log(str[i])
-// // }
-// let op=""
-// for(i=str.length-1;i>=0;i--){
-//   op=op+str[i];
-// }
-// console.log(op)
-//  let o=""
-//  for(i=1;i<=8;i++){
-//     o+="*"
-//     console.log(o) 
-//  }
-// console.log(o)
+ }
