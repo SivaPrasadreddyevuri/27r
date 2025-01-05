734b63483a39d7f4980e8a9ab5dbc8d165384756
SivaPrasadreddyevuri <evurisivaprasadreddy3@gmail.com>
2025-01-05 16:47:49 +0530

    new

--- index.js
+++ index.js
@@ -1,44 +1,74 @@
-let students=[
-    {id:1,s_name:"pavan",city:"hyd"},
-    {id:2,s_name:"arjun",city:"sec"},
-    {id:3,s_name:"meena",city:"vizag"},
-    {id:4,s_name:"rahul",city:"bengalore"},
-    {id:5,s_name:"sita",city:"chennai"},
-    {id:6,s_name:"vijay",city:"mumbai"}
-]
-//by using for in and for of loops
+    // let students=[
+    //     {id:1,s_name:"pavan",city:"hyd"},
+    //     {id:2,s_name:"arjun",city:"sec"},
+    //     {id:3,s_name:"meena",city:"vizag"},
+    //     {id:4,s_name:"rahul",city:"bengalore"},
+    //     {id:5,s_name:"sita",city:"chennai"},
+    //     {id:6,s_name:"vijay",city:"mumbai"}
+    // ]
+    // //by using for in and for of loops
 
 
-//for-in
-for(let i in students)
-{
-    console.log(students[i].s_name,"from",students[i].city);
-    // console.log(i);
-}
+    // //for-in
+    // for(let i in students)
+    // {
+    //     console.log(students[i].s_name,"from",students[i].city);
+    //     // console.log(i);
+    // }
 
 //for-of
- for(let i of students){
-    console.log(i.s_name,"from",i.city)
- }
+//  for(let i of students){
+//     console.log(i.s_name,"from",i.city)
+//  }
 
- for(i=0;i<=10;i++)
- {
-    console.log(`5x${i}=${i*5}`)
- }
- let str="Welcome"
- let str1=""
- for(i=str.length-1;i>=0;i--)
-{
-str1+=str[i]
+//  for(i=0;i<=10;i++)
+//  {
+//     console.log(`5x${i}=${i*5}`)
+//  }
+//  let str="Welcome"
+//  let str1=""
+//  for(i=str.length-1;i>=0;i--)
+// {
+// str1+=str[i]
+// // console.log(str1)
+// }
 // console.log(str1)
-}
-console.log(str1)
-st=""
-for(i=0;i<6;i++)
-{
-st+="*"
-    console.log(st)
-}
+// st=""
+// for(i=0;i<6;i++)
+// {
+// st+="*"
+//     console.log(st)
+// }
+// //task1 checking strng Palindrom or not:
+// let s_name="sivaavis"
+// let s1="sivaavis"
+// let op=""
+// for(i=s_name.length-1;i>=0;i--)
+// {
+//     op+=s_name[i]
+//     // console.log(s_name[i])
+// }
+// op===s1?console.log("s_name is palindrome"):console.log("not palindrome")
+
+// //task2
+// let a=["one","two","three","four"]
+// let b=""
+//  for(i=0;i<a.length;i++)
+//  {
+//     b+=a[i][1]
+//  }
+//  console.log(b)
+   
+//task3
+ let c=["one","two","three","four"]
+  for(i=0;i<c.length;i++)
+  {
+    console.log(c[i])
+    // for(i=c[i].length-1;i>=0;i--){
+
+    // }
+  }
+
 
 
 
