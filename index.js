8c0432ea0f5ec28a024ca7c7cccb56985aa63b76
https://github.com/SivaPrasadreddyevuri/27r.git <evurisivaprasadreddy3@gmail.com>
2025-01-12 17:48:41 +0530

    task

---
+++ index.js
@@ -0,0 +1,25 @@
+// level 1 tasks
+// checking Year is Leap Or Not
+// let year=prompt("Enter a Year for Checking Leapyear Or Not")
+// function isLeapOrNot()
+// {
+//     if(year%400===0){
+//         alert(` true ${year} is Leap year`)
+//     }
+//     else
+//     {
+//         alert(` false ${year} is Not a Leap Year`)
+//     }
+// }
+// isLeapOrNot();
+
+//Problem 4: Print Even Numbers Up to a Given Number
+let input=prompt("Enter a Positive Integer")
+function isEvenOrNot()
+{
+    if(input%2==0)
+    {
+        alert(`Entered ${input} is Even`);
+    }
+}
+isEvenOrNot();
