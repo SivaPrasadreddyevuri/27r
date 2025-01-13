97de70206066af2548035378514f5591c890e32b
https://github.com/SivaPrasadreddyevuri/27r.git <evurisivaprasadreddy3@gmail.com>
2025-01-13 18:30:04 +0530

    tasks

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // level 1 tasks
-// checking Year is Leap Or Not
+
+//problem 2: checking Year is Leap Or Not?
+
 // let year=prompt("Enter a Year for Checking Leapyear Or Not")
 // function isLeapOrNot()
 // {
@@ -13,14 +15,45 @@
 // }
 // isLeapOrNot();
 
-//Problem 4: Print Even Numbers Up to a Given Number
-let input=prompt("Enter a Positive Integer")
-let arr=[]
-function isEvenOrNot()
-{
-  for(i=arr.length-1;i>=0;i=i-2) 
-    {
-    } 
-    console.log(arr)
-}
-isEvenOrNot();
+//Problem 4: Print Even Numbers Up to a Given Number?
+
+// let input=prompt("Enter a Positive Integer")
+// let arr=[]
+// function isEvenOrNot()
+// {
+//   for(i=arr.length-1;i>=0;i=i-2) 
+//     {
+//     } 
+//     console.log(arr)
+// }
+// isEvenOrNot();
+
+
+
+
+
+//Problem 5: Multiplication Table? by taking input via prompt
+
+// function multiplicationTable()
+// {
+// let num=prompt("Enter Number For Getting Multiplication Table For that:")
+// for(i=1;i<=10;i++)
+// {
+// console.log(`${num}x${i}=${i*num}`)
+// }
+
+// }
+// multiplicationTable()
+
+
+//Problem 6: Greet User
+// Write a function greetUser that takes a name as a parameter and prints a greeting message. 
+// Call the function to greet a user whose name is entered by the user. (Use `prompt` to enter the user 
+// name).
+   
+// function wish(x)
+// {
+//   // console.log(`hello ${x}!`)
+//   alert(`Hello ${x}!`)
+// }
+// wish(prompt("Enter Name"))
