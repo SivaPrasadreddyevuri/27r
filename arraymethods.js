c8c8fd2e29e116174385e5b144a960ec3020e1ee
https://github.com/SivaPrasadreddyevuri/27r.git <evurisivaprasadreddy3@gmail.com>
2025-01-27 16:38:38 +0530

    new

--- arraymethods.js
+++ arraymethods.js
@@ -53,24 +53,34 @@ let arr=[1,4,5,"siva",7,9,12] // declaring array literal approach
 
 //find() method:
 
-let arr1=[2,6,9,"hi","siva","Apple" ]
-// let op=arr1.find(function(x,y,arr1)
-// {
-//    return x.length>4
+// let arr1=[2,6,9,"hi","siva","Apple" ]
+// // let op=arr1.find(function(x,y,arr1)
+// // {
+// //    return x.length>4
+// // })
+// // console.log(op)
+// let op=arr1.findLastIndex((value,index,arr1)=>{
+//     return value.length>2
 // })
 // console.log(op)
-let op=arr1.find((value,index,arr1)=>{
-    return
+
+let arr2=["hi","hello","welcome","Done"]
+
+// let op=arr2.map((value,index,arr2)=>{
+// // console.log(arr2[1])
+// console.log (value+2)
+// })
+
+// console.log(op)
+// let b=arr2.map((x,y,arr2)=>{
+    
+//    return (x.length+3)
+// })
+// console.log(b)
+
+arr2.map((x,y,arr2)=>{
+     console.log(x.slice(0,-1)+x.at(-1).toUpperCase())
 })
-
-
-
-
-
-
-
-
-
 
 
 
