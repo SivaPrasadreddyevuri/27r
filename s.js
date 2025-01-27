b341b345fcbe79b29d97946438c727aa1691ed1d
https://github.com/SivaPrasadreddyevuri/27r.git <evurisivaprasadreddy3@gmail.com>
2025-01-28 00:41:46 +0530

    new

--- s.js
+++ s.js
@@ -4,11 +4,34 @@ for(i=0;i<a.length;i++)
 {
    if(a[i]==a[i].toUpperCase())
    {
-     b[b.length]=a[i].toLowerCase()
+   b.push(a[i].toLowerCase())
    }
    else
    {
-    b[b.length]=a[i].toUpperCase()
+    b.push(a[i].toUpperCase())
    }
 }
 console.log(b)
+
+       //OR
+
+      //  for(i=0;i<a.length;i++)
+      //   {
+      //      if(a[i]==a[i].toUpperCase())
+      //      {
+      //      b[b.length]=a[i].toLowerCase()
+      //      }
+      //      else
+      //      {
+      //       b[b.length]=a[i].toUpperCase()
+      //      }
+      //   }
+      //   console.log(b)    
+
+
+
+
+      // 1.What will the following code output?
+   // const arr = [1, 2, 3, 4];
+   // const result = arr.map((num, index, array) => array[index] * 2);
+   // console.log(result);
