7d0625d50ebea1eaa47d6280a703f4d6f7e74b4f
https://github.com/SivaPrasadreddyevuri/27r.git <evurisivaprasadreddy3@gmail.com>
2025-01-27 15:13:50 +0530

    new

--- s.js
+++ s.js
@@ -1,3 +1,14 @@
-let a=8
-
-console.log(a)
+let a=["html","css","JAVASCRIPT","react","ANGULARJS"]
+let b=[]
+for(i=0;i<a.length;i++)
+{
+   if(a[i]==a[i].toUpperCase())
+   {
+     b[b.length]=a[i].toLowerCase()
+   }
+   else
+   {
+    b[b.length]=a[i].toUpperCase()
+   }
+}
+console.log(b)
