baa598d87ff4337b9a38a956f2995bdc5ecc12cd
https://github.com/SivaPrasadreddyevuri/27r.git <evurisivaprasadreddy3@gmail.com>
2025-01-30 08:11:45 +0530

    new

---
+++ numbermethods.js
@@ -0,0 +1,25 @@
+// let num=4.36
+// let k=num.toPrecision(3) // for deciding the length of a number
+// console.log(k)  // it gives output in the fornm of string
+
+
+// let r=num.toFixed(1) // decides decimal's we need
+// console.log(r)
+
+
+// let g=parseInt(5.75)
+// console.log(g)
+// console.log(typeof g)
+
+// let f=parseFloat()
+// console.log(f)
+// console.log(typeof f)
+
+let x=Math.random()*1000
+let y=parseInt(x)
+if(y<10000){
+    console.log(y+10000)
+}
+else{
+console.log(y)}
+
